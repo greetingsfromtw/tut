6e2884a40730050e4148ab33a3cfc7e0cdfb695b
greetingsfromtw <greetingsfromtw@gmail.com>
2016-03-27 11:51:12 +0800

    testing jade mixin

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.set('views',__dirname + '/views');
+app.set('view engine','jade');
+
 
 app.use(express.static(__dirname + '/public'));
 
-
 app.get('/route',function(req,res){
 	res.send('this is a route');
+})
+
+app.get('/hello',function(req,res){
+	res.render('hello');
 })
 
 app.listen(3000);
